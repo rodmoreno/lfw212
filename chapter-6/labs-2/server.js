80b9cadea717e001c407c21eecf2557f2ce0cbcb
Rodrigo Moreno <rodrigo@paywithflux.com>
2021-05-09 13:39:30 -0500

    feat(Chapter 6 - Labs 2): add delete endpoint to delete existing boats

---
+++ chapter-6/labs-2/server.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+
+const { boat } = require('./model')
+const { uid } = boat
+
+app.use(express.json())
+
+app.post('/boat', (req, res, next) => {
+    boat.create(uid(), req.body.data, (err, id) => {
+        if (err) {
+            next(err)
+            return
+        }
+
+        res.status(201).json({ id })
+    })
+})
+
+app.delete('/boat/:id', (req, res, next) => {
+  boat.del(req.params.id, (err) => {
+    if (err) {
+      if (err.message === 'not found') {
+        next()
+        return
+      }
+
+      next(err)
+      return
+    }
+
+    res.status(204).end()
+  })
+})
+
+app.get('/boat/:id', (req, res, next) => {
+    boat.read(req.params.id, (err, data) => {
+        if (err) {
+            if (err.message === 'not found') {
+                next()
+                return
+            }
+
+            next(err)
+            return
+        }
+
+        res.json(data)
+    })
+})
+
+app.use((_req, res) => {
+    res.status(404).json({ message: 'not found' })
+})
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status ?? 500).json({ message: 'internal server error' })
+})
+
+app.listen(process.env?.PORT ?? 3000)
+
